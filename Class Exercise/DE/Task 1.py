d861ed03696d6ed3a6b0c872d68d38fe3e381896
Tony Karimpumannil <40309@longroad.ca.uk>
2015-02-10 15:11:03 +0000

    PUSH

--- Class Exercise/DE/Task 1.py
+++ Class Exercise/DE/Task 1.py
@@ -6,7 +6,7 @@ import pickle
 with open ("task 1.bat" , mode = "wb") as my_file:
     user = []
     user_name = input("Please enter our full name: ")
-    user.append(user_name)
     user_dob = input("Please enter your date of birth (YYYY-MM-DD): ")
+    user.append(user_name)
     user.append(user_dob)
     pickle.dump(user, my_file)
