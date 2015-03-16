732a5e1ca5e5cae030e4f31188b318fa244200ea
Tony Karimpumannil <40309@longroad.ca.uk>
2015-03-16 12:15:52 +0000

    push

--- Spotcheck/Spot Check - Attempt 2.py
+++ Spotcheck/Spot Check - Attempt 2.py
@@ -14,10 +14,9 @@ def load_games():
     try:
         with open("games.dat", mode="rb") as my_file:
             file_data = pickle.load(my_file)
-            for line in file_data:
-                print(line)
     except ValueError:
         print("The file requested does not exist")
+    return file_data
     pass
 
 def save_games(games):
@@ -29,18 +28,23 @@ def save_games(games):
 
 #the parameter is games because eventually you will be displaying
 #multiple games using this function
-def display_games(games):
+def display_games(games, file_data):
     print()
+    print("-"*79)
+    print("|{0:<10}|{1:<15}|{2:<8}|{3:<6}|{4:<13}|{5:<20}|".format("Name", "Platform","Genre","Cost","No of Players","Online Functionality"))
+    print("-"*79)
     for count in range(len(games)):
         for each in range(1):
-            print()
-            print("Name: {0}".format(games[count-1][0]))
-            print("Platform: {0}".format(games[count-1][1]))
-            print("Genre: {0}".format(games[count-1][2]))
-            print("Cost: {0}".format(games[count-1][3]))
-            print("Number of Players: {0}".format(games[count-1][4]))
-            print("Online Functionality: {0}".format(games[count-1][5]))
+            print("|{0:<10}|{1:<15}|{2:<8}|{3:<6}|{4:<13}|{5:<20}|".format(games[count-1][0],games[count-1][1],games[count-1][2],games[count-1][3],games[count-1][4],games[count-1][5]))
+            print("-"*79)
+    for count_2 in range(len(file_data)):
+        for each_2 in range(1):
+            print("|{0:<10}|{1:<15}|{2:<8}|{3:<6}|{4:<13}|{5:<20}|".format(file_data[count_2-1][0],file_data[count_2-1][1],file_data[count_2-1][2],file_data[count_2-1][3],file_data[count_2-1][4],file_data[count_2-1][5]))
+            print("-"*79)
     pass
+
+
+
 
 def get_game_from_user(games):
     checker = False
@@ -93,20 +97,20 @@ def main():
             games = get_game_from_user(games)
             pass
         elif selected_option == 2:
-            display_games(games)
+            display_games(games, file_data)
             pass
         elif selected_option == 3:
             save_games(games)
             exit_program = True
             pass
         elif selected_option == 4:
-            load_games()
+            file_data = load_games()
             pass
         elif selected_option == 5:
             exit_program = True
             pass
         else:
-            print("Please enter a valid option (1-3)")
+            print("Please enter a valid option (1-5)")
             print()
 
 if __name__ == "__main__":
